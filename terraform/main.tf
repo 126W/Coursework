c057eea6e3f0e67b7768a1b5a1c603cbab413155
Evgeny.Shakhov <122415129+126W@users.noreply.github.com>
2023-07-11 02:31:35 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,7 +19,9 @@ terraform {
 }
 #==провайдер
 provider "yandex" {
-  zone = "ru-central1-a"
+  token        = var.yc_token
+  cloud_id     = var.yc_cloud_id
+  folder_id    = var.yc_folder_id
 }
 
 #==сети
